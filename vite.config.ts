24936c5d76899a2683899b819cf58e9796db4e9f
Thafany Passos <thafany.passos.dev2024@gmail.com>
2026-04-21 18:53:59 -0300

    mudanças vite para offiline

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',   
+  plugins: [
+    react(),
+    viteSingleFile(),
+  ],
+  base: './',
+  build: {
+    assetsInlineLimit: 100_000_000,
+    chunkSizeWarningLimit: 100_000_000,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
 })
