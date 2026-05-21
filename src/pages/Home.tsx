b8af2c000a4f0d156ba7c42a63239339df008c7f
ThaPassos <Thafany.passos.dev2024@gmail.com>
2026-05-21 12:06:10 -0300

    mudnaça na logo e ordem dos elementos no Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Play } from "lucide-react";
 import { sounds } from "../lib/sounds";
 import BotaoSom from "../components/BotaoSom";
 import AnimatedLines from "../components/LinhasAnimadas";
-import logoCondutti from "../assets/logoFundoEscuro.png";
+import logoCondutti from "../assets/logoCondutti.png";
 import qrCode1 from "../assets/qrcode2.png";
 
 export default function Home() {
@@ -29,26 +29,18 @@ export default function Home() {
           <img
             src={logoCondutti}
             alt="Condutti Cabos Especiais"
-            className="h-auto w-56 drop-shadow-[0_8px_24px_rgba(0,0,0,0.45)] sm:w-72"
+            className="w-[280px] sm:w-[350px] h-auto object-contain drop-shadow-[0_8px_24px_rgba(0,0,0,0.45)]"
           />
         </div>
 
         <div className="h-px w-4/5 bg-border" />
 
-        <h1 className="animate-title text-4xl font-black leading-tight tracking-tight sm:text-6xl">
+        <h1 className="animate-title text-4xl font-black leading-tight tracking-tight sm:text-6xl mt-[40px]">
           JOGO DA <span className="text-accent">MEMÓRIA</span>
         </h1>
-        <p className="text-base text-foreground/80 sm:text-xl">Encontre os pares e vença!</p>
+        <p className="text-base text-foreground/80 sm:text-xl mb-[40px]">Encontre os pares e vença!</p>
 
-        <Link
-          to="/jogo"
-          onClick={() => sounds.click()}
-          className="group mt-4 inline-flex items-center gap-3 rounded-full bg-primary px-10 py-4 text-lg font-bold text-primary-foreground shadow-btn transition-all hover:scale-105 hover:bg-accent active:translate-y-1 active:shadow-btn-active sm:px-14 sm:py-5 sm:text-2xl"
-        >
-          <Play className="h-5 w-5 fill-current transition-transform group-hover:translate-x-0.5 sm:h-6 sm:w-6" />
-          JOGAR
-        </Link>
-
+        
         {/* QR Codes */}
         <div className=" flex flex-wrap justify-center gap-8">
           {/* QR Code 1 */}
@@ -69,6 +61,16 @@ export default function Home() {
           </div>
 
         </div>
+
+        <Link
+          to="/jogo"
+          onClick={() => sounds.click()}
+          className="group mt-4 inline-flex items-center gap-3 rounded-full bg-primary px-10 py-4 text-lg font-bold text-primary-foreground shadow-btn transition-all hover:scale-105 hover:bg-accent active:translate-y-1 active:shadow-btn-active sm:px-14 sm:py-5 sm:text-2xl mt-[40px]"
+        >
+          <Play className="h-5 w-5 fill-current transition-transform group-hover:translate-x-0.5 sm:h-6 sm:w-6" />
+          JOGAR
+        </Link>
+
       </div>
     </div>
   );
